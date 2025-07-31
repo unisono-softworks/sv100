d836293daea5b6d62825c31cfb9b2832dc259435
Dennis Heiden <dennis-heiden@unisono.rocks>
2025-07-31 15:41:01 +0200

    post_content - remove legacy css

--- lib/modules/sv_block_post_content/sv_block_post_content.php
+++ lib/modules/sv_block_post_content/sv_block_post_content.php
@@ -1,32 +1,17 @@
 <?php
-	namespace sv100;
 
-	class sv_block_post_content extends init {
-		public function init() {
-			$this->set_module_title( __( 'Block: Post Content', 'sv100' ) )
-				->set_module_desc( __( 'Settings for Gutenberg Block', 'sv100' ) )
-				//->set_css_cache_active()->set_block_handle('wp-block-post-content')
-				->set_block_name('core/post-content');
+namespace sv100;
 
-			if(!is_admin()){
-				add_action('wp', array($this, 'has_classic_block'));
-			}
-		}
-		public function register_scripts() {
-			parent::register_scripts();
-
-			$this->get_script('legacy')
-				->set_path('lib/css/common/legacy.css')
-				->set_inline();
-
-			return $this;
-		}
-
-		public function has_classic_block(){
-			if(has_blocks()){
-				$this->get_script('legacy')->set_is_enqueued(false);
-			}else{
-				$this->get_script('legacy')->set_is_enqueued();
-			}
-		}
+class sv_block_post_content extends init {
+	public function init() {
+		$this->set_module_title(__('Block: Post Content', 'sv100'))
+			->set_module_desc(__('Settings for Gutenberg Block', 'sv100'))
+			//->set_css_cache_active()->set_block_handle('wp-block-post-content')
+			->set_block_name('core/post-content');
 	}
+	
+	public function register_scripts(): sv_block_post_content{
+		parent::register_scripts();
+		return $this;
+	}
+}
